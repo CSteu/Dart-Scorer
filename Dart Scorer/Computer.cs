1c5a067015f940ecc5bc8e26863c597abaa74174
DSU <DSU@PF2VC3AY>
2023-04-16 15:52:51 -0500

    Add project files.

---
+++ Dart Scorer/Computer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dart_Scorer
+{
+    public class Computer
+    {
+        int level;
+
+        public Computer()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Level
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+
+}
---
+++ Dart Scorer/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dart_Scorer
+{
+    public class User
+    {
+        public string firstName;
+        public string lastName;
+        public string username;
+        private string _password;
+
+        public User()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void login()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Password
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
