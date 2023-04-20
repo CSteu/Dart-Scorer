ed594367b5a66b2c101eca5916789f6f9ab1df7a
Chase Steuerwald <Chase.Steuerwald@trojans.dsu.edu>
2023-04-20 13:55:27 -0500

    4/20

---
+++ Dart Scorer/LoginForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dart_Scorer
+{
+    public class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+
+            // TODO: Check the user's credentials against a database or other authentication mechanism
+
+            if (username == "admin" && password == "password")
+            {
+                MessageBox.Show("Login successful!");
+                this.DialogResult = DialogResult.OK;
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Invalid username or password");
+            }
+        }
+    }
+}
--- Dart Scorer/Match.Designer.cs
+++ Dart Scorer/Match.Designer.cs
@@ -28,12 +28,130 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn301 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.btn701 = new System.Windows.Forms.Button();
+            this.btn501 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(95, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "VS. Computer";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(92, 51);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(188, 50);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(248, 65);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(22, 21);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(92, 148);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(364, 380);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label2.Location = new System.Drawing.Point(92, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 37);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Players:";
+            // 
+            // btn301
+            // 
+            this.btn301.Location = new System.Drawing.Point(523, 51);
+            this.btn301.Name = "btn301";
+            this.btn301.Size = new System.Drawing.Size(140, 50);
+            this.btn301.TabIndex = 5;
+            this.btn301.Text = "301";
+            this.btn301.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(391, 116);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(65, 26);
+            this.numericUpDown1.TabIndex = 6;
+            // 
+            // btn701
+            // 
+            this.btn701.Location = new System.Drawing.Point(815, 51);
+            this.btn701.Name = "btn701";
+            this.btn701.Size = new System.Drawing.Size(140, 50);
+            this.btn701.TabIndex = 7;
+            this.btn701.Text = "701";
+            this.btn701.UseVisualStyleBackColor = true;
+            // 
+            // btn501
+            // 
+            this.btn501.Location = new System.Drawing.Point(669, 51);
+            this.btn501.Name = "btn501";
+            this.btn501.Size = new System.Drawing.Size(140, 50);
+            this.btn501.TabIndex = 8;
+            this.btn501.Text = "501";
+            this.btn501.UseVisualStyleBackColor = true;
+            // 
+            // Match
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1038, 576);
+            this.Controls.Add(this.btn501);
+            this.Controls.Add(this.btn701);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.btn301);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Match";
             this.Text = "Match";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn301;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button btn701;
+        private System.Windows.Forms.Button btn501;
     }
 }
